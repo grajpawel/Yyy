326ac4b4d3050ba7e35d753679574d57687243a1
grajpawel <32206069+grajpawel@users.noreply.github.com>
2018-05-24 19:55:00 +0200

    Added first version

---
+++ Assets/1/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+	public static int score;
+	public Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreText.text = score.ToString ();
+
+	}
+	public void OnTap(){
+		score++;
+	}
+}
---
+++ Assets/2/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public Text ScoreText;
+	private float timer;
+	public static bool TimerOn;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		TimerOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreText.text = timer.ToString ("F4");
+
+		if (TimerOn == true) {
+			timer += Time.deltaTime;
+		}
+	}
+	public void OnTap(){
+		if (TimerOn == false)
+			TimerOn = true;
+		else
+			TimerOn = false;
+	}
+}
